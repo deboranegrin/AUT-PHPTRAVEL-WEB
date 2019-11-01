48e30d6a92fb09f008537621dfda4fb4141787a2
Debora Negrin <deboracnc@yahoo.com.br>
2019-11-01 11:10:48 -0300

    Aula Final 1.1

--- src/test/java/curso/treinamento/utils/JSHelper.java
+++ src/test/java/curso/treinamento/utils/JSHelper.java
@@ -1,29 +1,29 @@
-package curso.treinamento.utils;
-
-import java.util.List;
-import java.util.Map;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import curso.treinamento.setup.Hooks;
-import io.cucumber.datatable.DataTable;
-
-public class JSHelper {
-
-public static void scroll_pagina(int x, int y) {
-		
-	JavascriptExecutor js = (JavascriptExecutor) Hooks.getDriver()
-		
-	}
-
-public static void scroll_elemento(String css, int x, int y) {
-	String scriptJS = "document.querySelector"('" + css + "').scrollBy(0, 1000);
-	
-
-			
-
-}
-}
+//package curso.treinamento.utils;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//
+//import curso.treinamento.setup.Hooks;
+//import io.cucumber.datatable.DataTable;
+//
+//public class JSHelper {
+//
+//public static void scroll_pagina(int x, int y) {
+//		
+//	JavascriptExecutor js = (JavascriptExecutor) Hooks.getDriver()
+//		
+//	}
+//
+//public static void scroll_elemento(String css, int x, int y) {
+//	String scriptJS = "document.querySelector"('" + css + "').scrollBy(0, 1000);
+//	
+//
+//			
+//
+//}
+//}
